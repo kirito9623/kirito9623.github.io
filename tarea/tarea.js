8e6ffadf869634c6d7ac19bce177cbbe183b290f
Roberto Gonzalez Carranza <rgc00023@gmail.com>
2021-05-26 17:34:33 -0500

    Tarea 1 Problemas Secuenciales Javascript

---
+++ tarea/tarea.js
@@ -0,0 +1,191 @@
+
+let b = 10;
+let h = 20;
+let A = 0;
+A = b*h/2;
+console.log(`El valor de area del triangulo es ${A} `);
+document.write(`El valor de area del triangulo es ${A} `);
+
+
+
+let pesos = 1000;
+let precioDolar = 3.25;
+let dolares = 0;
+dolares = pesos*precioDolar;
+console.log(`Puedo comprar ${dolares} dolares`);
+document.write(`Puedo comprar ${dolares} dolares`);
+
+
+let añoNacimiento = 1996;
+let añoActual = 2021;
+let edad = 0;
+edad = añoActual -  añoNacimiento;
+console.log(`La edad es ${edad} `);
+document.write(`La edad es ${edad} `);
+
+
+
+
+
+let m2 = 10;
+let precioMetroCuadrado = 50;
+let presupuesto = 0;
+presupuesto = m2 * precioMetroCuadrado;
+console.log(`El presupuesto es ${presupuesto}`);
+document.write(`El presupuesto es ${presupuesto}`);
+
+
+
+
+let A = 10;
+let B = 5;
+let C = 0;
+C = ((A**2) + (B**2)) ** 0.5;
+console.log(`La hipotenusa es ${C}`);
+document.write(`La hipotenusa es ${C}`);
+
+
+
+let kilometro = 10;
+let costoKilometro = 5;
+let total = 0;
+total = kilometro * costoKilometro;
+console.log(`El costo total es el ${total}`);
+document.write(`El costo total es el ${total}`);
+
+
+let velocidad = 10;
+let distancia = 50;
+let tiempo = 0;
+tiempo = distancia / velocidad;
+console.log(`El tiempo es  ${tiempo} segundos`);
+document.write(`El tiempo es ${tiempo} segundos`);
+
+
+let minutos = 10;
+let costoMinuto = 2;
+let costoTotal = 0;
+costoTotal = minutos * costoMinuto;
+console.log(`El costo total es  ${costoTotal}`);
+document.write(`El costo total es  ${costoTotal}`);
+
+
+
+let consumo = 10;
+let costoMetro3 = 2;
+let costoTotal = 0;
+costoTotal = consumo * costoMetro3;
+console.log(`El costo total es  ${costoTotal}`);
+document.write(`El costo total es  ${costoTotal}`);
+
+
+
+let kw = 10;
+let costoKw = 2;
+let pagoTotal = 0;
+pagoTotal = kw * costoKw;
+console.log(`El pago total es  ${pagoTotal}`);
+document.write(`El pago total es  ${pagoTotal}`);
+
+
+
+
+let descuento = 0.20;
+let precio = 200;
+let Iva = 0.15;
+let precioConDescuento = 0;
+let pago = 0;
+
+precioConDescuento = precio - (precio * descuento);
+pago = precioConDescuento + (precioConDescuento * Iva)
+
+console.log(`El precio con descuento es  ${precioConDescuento}`);
+console.log(`El pago total es  ${pago}`);
+document.write(`El precio con descuento es  ${precioConDescuento} `);
+document.write(`El pago total es  ${pago} `);
+
+
+
+let sueldo = 10;
+let ahorro = 0;
+ahorro = (sueldo * 0.15 ) * 4 * 12;
+console.log(`El ahorro es ${ahorro}`);
+document.write(`El ahorro es ${ahorro}`);
+
+
+
+
+let dias = 10;
+let hotel = 300;
+let comida = 250;
+let monto = 0;
+monto = ( hotel + comida + 10000 ) * dias ;
+console.log(`El monto total es  ${monto}`);
+document.write(`El monto total es  ${monto}`);
+ 
+
+
+
+let I = 10;
+let R = 4;
+let V = 0;
+let P = 0;
+V = I * R ;
+P = V * I ;
+
+console.log(`La potencia es  ${P}`);
+document.write(`La potencia es  ${P}`);
+ 
+
+
+let a = 10;
+let b = 4;
+let A = 0;
+A = a * b ;
+console.log(`El area del cuadrado es  ${A}`);
+document.write(`El area del cuadrado es  ${A}`);
+
+
+
+
+let a = 15;
+let b = 14;
+let c = 18;
+let promedio = 0;
+promedio = (a + b + (2*c) )/4;
+console.log(`La nota promedio es  ${promedio}`);
+document.write(`La nota promedio es  ${promedio}`);
+
+
+
+let años = 5; 
+let edadSemanas = 0;
+let edadMeses = 0;
+let edadDias =0;
+let edadHoras =0;
+edadMeses = años * 12;
+edadSemanas = edadMeses * 4;
+edadDias = edadSemanas * 7;
+edadHoras = edadDias * 24;
+console.log(`La edad es  ${edadMeses} meses ${edadSemanas} semanas ${edadDias} dias ${edadHoras} horas `);
+document.write(`La edad es  ${edadMeses} meses ${edadSemanas} semanas ${edadDias} dias ${edadHoras} horas `);
+
+
+
+
+let minutos = 10;
+let costoMinuto = 2;
+let costoTotal = 0;
+costoTotal = minutos * costoMinuto;
+console.log(`El costo total es  ${costoTotal}`);
+document.write(`El costo total es  ${costoTotal}`);
+
+
+
+
+let dias = 10;
+let precioDia = 35;
+let cobro = 0;
+cobro = precioDia * dias;
+console.log(`El costo total es  ${cobro}`);
+document.write(`El costo total es  ${cobro}`);
